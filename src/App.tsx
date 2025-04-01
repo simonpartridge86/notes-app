f456a67f27fa10136fc8dd1b912f4b340b604048
simonpartridge86 <simonpartridge86@gmail.com>
2025-04-01 11:39:35 +0100

    add initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import "./App.css";
+import "./global.css";
 
 function App() {
-  return <div>NOTES</div>;
+  return (
+    <main className="app-container">
+      <header className="app-header">
+        <h1 className="page-title">Notes</h1>
+      </header>
+      <section className="notes-list">Notes go here</section>
+    </main>
+  );
 }
 
 export default App;
